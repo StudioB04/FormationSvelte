d3bd4d46e7b5aa2fb98d364cccc90314a6c911fb
Thomas PIERRE <thomass.pierre@me.com>
2024-12-14 15:19:13 +0100

    init theme

---
+++ components/SlideFooter.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const today = new Date();
+
+const day = [
+  "Lundi",
+  "Mardi",
+  "Mercredi",
+  "Jeudi",
+  "Vendredi",
+  "Samedi",
+  "Dimanche",
+][today.getDay()];
+
+const date = ("0" + today.getDate()).slice(-2);
+
+const month = [
+  "Janvier",
+  "Février",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Juin",
+  "Juillet",
+  "Août",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre",
+][today.getMonth()];
+
+const year = today.getFullYear();
+</script>
+
+<template>
+  <footer class="absolute bottom-0 left-0 w-full bg-slate-800">
+    <hr class="border-slate-900" />
+
+    <div
+      class="flex gap-2 justify-end items-center px-2 py-1 text-2 text-slate-200"
+    >
+      <div class="w-full flex items-center gap-2">
+        <img src="/assets/logo-studiob04.svg" alt="" class="w-4" />
+        <span>StudioB04</span>
+        <small>|</small>
+        <span>{{ day }} {{ date }} {{ month }} {{ year }} </span>
+        <span class="mis-auto">
+          Diapo <SlideCurrentNo /> sur <SlidesTotal />
+        </span>
+      </div>
+    </div>
+  </footer>
+</template>
