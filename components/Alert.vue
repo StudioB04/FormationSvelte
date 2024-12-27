4055f8080a8792b6aafc12fa87e22601e6593008
Thomas PIERRE <thomass.pierre@me.com>
2024-12-27 17:09:50 +0100

    fix Alert

--- components/Alert.vue
+++ components/Alert.vue
@@ -4,8 +4,7 @@
   export default {
     name: "Alert",
     props: {
-      icon: { type: String, required: false },
-      color: { type: String, required: false },
+      type: { type: String, required: false },
       title: { type: String, required: false },
       text: { type: String, required: true },
     },
@@ -18,11 +17,34 @@
 </script>
 
 <template>
-  <blockquote flex flex-col>
-    <div :class="`text-${color}`">
-      <carbon-information v-if="icon"/> 
+  <blockquote :class="`Alert Alert--${type}`">
+    <header>
+      <carbon-idea v-if="type === 'idea'"/> 
+      <carbon-checkmark v-if="type === 'success'"/> 
+      <carbon-information v-if="type === 'information'"/> 
+      <carbon-warning v-if="type === 'warning'"/> 
+      <carbon-warning v-if="type === 'danger'"/> 
       {{title}}
-    </div>
+    </header>
     <div v-html="renderedText"></div>
   </blockquote>
 </template>
+
+<style lang="css" scoped>
+  .Alert{
+    display: flex;
+    flex-direction: column;
+    border-inline-start-color: var(--color);
+
+    header {
+      color: var(--color);
+    }
+  }
+
+
+  .Alert--idea { --color:  var(--green-500); }
+  .Alert--success { --color:  var(--green-500); }
+  .Alert--information { --color:  var(--blue-500); }
+  .Alert--warning { --color:  var(--orange-500); }
+  .Alert--danger { --color:  var(--red-500); }
+</style>
