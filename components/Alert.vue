bc1c4f1bc3165efc156b548d02752561254b3b80
Thomas PIERRE <thomass.pierre@me.com>
2024-12-27 16:00:27 +0100

    dev jour 2 en cours

---
+++ components/Alert.vue
@@ -0,0 +1,28 @@
+<script>
+  import { marked } from "marked"; 
+
+  export default {
+    name: "Alert",
+    props: {
+      icon: { type: String, required: false },
+      color: { type: String, required: false },
+      title: { type: String, required: false },
+      text: { type: String, required: true },
+    },
+    computed: {
+      renderedText() {
+        return marked(this.text || "");
+      },
+    },
+  };
+</script>
+
+<template>
+  <blockquote flex flex-col>
+    <div :class="`text-${color}`">
+      <carbon-information v-if="icon"/> 
+      {{title}}
+    </div>
+    <div v-html="renderedText"></div>
+  </blockquote>
+</template>
